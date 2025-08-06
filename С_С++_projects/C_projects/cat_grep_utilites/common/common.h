b9d38c527e9ab7a6fe7e631851fe60389dfa697a
Екатерина Киракосян <38876588+igrein@users.noreply.github.com>
2025-08-06 14:11:09 +0300

    Create common.h
    заголовочный

---
+++ С_С++_projects/C_projects/cat_grep_utilites/common/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void PrintUsage(const char* program_name);
+
+#endif
