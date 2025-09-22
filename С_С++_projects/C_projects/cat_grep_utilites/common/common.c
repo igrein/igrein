c564628b6ca040f0b931d0a29bb8caa0def06e4b
Екатерина Киракосян <38876588+igrein@users.noreply.github.com>
2025-09-22 22:36:02 +0300

    Delete С_С++_projects directory

--- С_С++_projects/C_projects/cat_grep_utilites/common/common.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-void PrintUsage(const char* program_name);
-
-void PrintUsage(const char* program_name) {
-  fprintf(stderr, "Usage: %s [options] [arguments]\n", program_name);
-}
